5d032c9e84a6e28bd5f8f6f2f7c76be7c080c41b
Acheedella1 <cheedellalucky@gmail.com>
2024-10-22 23:23:09 -0500

    Update HW 9.sql

--- HW 9.sql
+++ HW 9.sql
@@ -47,23 +47,21 @@ SELECT film.title,
 FROM film  
 WHERE film.length IS NOT NULL;
 --Q9
---I used the explain function for Q7's query and Q2's query. Q7's query returns that the hash aggregate is 
---"HashAggregate  (cost=571.66..571.69 rows=3 width=13)" This means that it took a total amount of 571.66-571.69 seconds
---and resources. Then the rows mean that there are 3 rows and the average size of bytes used was 13. The 
---Group Key was status meaning that the data was grouped by status. The sequential scan is where the database reads each 
---thing till it matches the group by functions and that 491.99 is the estimated cost(resources and time) used.
---There are 16,044 rows that match the conditions.For Q2 the explain feature returned "Aggregate  (cost=32.60..32.61 rows=1 width=32)"
---Nested Loop  (cost=13.09..32.47 rows=49 width=6),Nested Loop  (cost=12.80..28.93 rows=2 width=4), ->  Hash Join  
---(cost=12.53..28.15 rows=2 width=4), Hash Cond: (a.city_id = t.city_id), Seq Scan on address a  
---(cost=0.00..14.03 rows=603 width=6),Hash  (cost=12.50..12.50 rows=2 width=4), Seq Scan on city t  
---(cost=0.00..12.50 rows=2 width=4), Filter: ((city)::text = ANY ('{""Kansas City"",""Saint Louis""}'
---::text[])),Index Scan using idx_fk_address_id on customer c  (cost=0.28..0.38 rows=1 width=6)"
---Index Cond: (address_id = a.address_id),Index Scan using idx_fk_customer_id on payment p  (
---cost=0.29..1.53 rows=24 width=8)Index Cond: (customer_id = c.customer_id). This means that the aggregate
---had an estimated cost of 32.60-32.61 and that it returned one row with 32 bytes. The nested loop returned
---in the same format but because its a nested loop we have to keep in mind it combines 2 rows. There is another nested
---loop with the cost, row, and bytes. the next hash join was on a.city_id and t.city_id and it returned with
---cost,row, and bytes used.Then the sequential scan did an overall scan of all of the cost,row, and bytes.
--- Then it had a filter explained by the condition set for it to only be people in kansas city and saint louis.
---A index scan is performed with a condition meaning that the program was looking at columns like the condition. 
---That was repeated again and thats it. 
+EXPLAIN 
+SELECT status, COUNT(*) AS film_num
+FROM rental
+GROUP BY STATUS;
+EXPLAIN 
+SELECT SUM(p.amount)
+FROM payment.p
+LEFT JOIN customer c ON p.customer_id = c.customer_id
+LEFT JOIN address a ON c.address_id = a.address_id
+LEFT JOIN city t ON a.city_id = t.city_id
+WHERE t.city IN ('Kansas City','Saint Louis')
+--Under the explain function of Q7 I got a combination of hash agg, group keys, and sequential scans. All of them return 
+--(work, rows, and bytes consumed).The work is essentially the amount of time and space consumed in the dataframe. The hash 
+--aggregate essentially shows us grouped by columns. The Group Key was just showing that we had a secure connection.Then there was 
+--a sequencial scan that gave us a good summary of all of the work, rows ,and bytes consumed. For the Q2, I got a combination of nested
+--loops, aggregate functions, sequencial scans, index scans, and filters. The filters were the imposed conditions in the code. The index 
+--scans were just telling us that SQL had to scan multiple tables to check for the conditions I was imposing. Nested loops were just 
+--about the conditions I had withing a condition. 
